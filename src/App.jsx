d11e91bafb5c9e0b2d4d2abe1730430afa05e88e
Navin Ayer <ayernavin11@gmail.com>
2026-01-01 16:17:02 +0545

    Add, Select and Delete features added

--- src/App.jsx
+++ src/App.jsx
@@ -4,68 +4,51 @@ import  TodoList  from './components/TodoList'
 
  const App = () => {
 
-  const [addInput, setAddInput] = useState("")
-  const [todos, setTodos] = useState([])
-  const [editTodo, setEditTodo] = useState(null)
-  const [editText, setEditText] = useState("")
 
-  const addInputOnSearch = (e)=> {
+const [addInput, setAddInput] = useState("")
+const [todos, setTodos] = useState([])
 
-    setAddInput(e.target.value)
-  }
 
-  const addTodos = ()=>{
-    const newTodo = {
-      id : Date.now(),
-      text: addInput,
-      completed : false
-    }
-    setTodos([...todos, newTodo])
-    setAddInput("")
-  }
-
-  const handleKey = (e)=>{
-    if(e.key === "Enter"){
-      addTodos()
-    }
-  }
-
-  const todoCompleted = (id)=>{
-    setTodos((currTodo)=>
-     currTodo.map((todo)=>
-     todo.id === id ? {...todo, completed: !todo.completed} : todo
-    )
-    )
-  }
+const addInputOnSearch = (e)=>{
+  setAddInput(e.target.value)
+}
  
-
-  const deleteTheTodo = (id)=>{
-    setTodos((currTodo)=>
-     currTodo.filter((todo)=>
-    todo.id !== id
-     )
-    )
+const addTodo = ()=>{
+  const newTodo = {
+    id: Date.now(),
+      text: addInput,
+      completed: false
   }
+  setTodos([...todos, newTodo])
+  setAddInput("")
+}  
 
-  const startEditing = (id, currText)=>{
-    setEditTodo(id)
-    setEditText(currText)
-  }
-
-  const saveEdit = (id) =>{
-    setTodos((currTodo)=>
+const todoCompleted = (id)=>{
+  setTodos((currTodo)=>
    currTodo.map((todo)=>
-   todo.id === id ? {...todo, text: editText} : todo
+   todo.id === id ? {...todo, completed : !todo.completed} : todo
   )
   )
-  setEditTodo(null)
-  setEditText("")
+}
+
+const handleKey = (e)=>{
+  if(e.key === "Enter"){
+    addTodo()
   }
+}
+
+const deleteTheTodo = (id)=>{
+  setTodos((currTodo)=>
+   currTodo.filter((todo)=>
+   todo.id !== id
+  )
+  )
+}
     
   return (
-    <div>
-      <TodoInput addInputOnSearch = {addInputOnSearch} value = {addInput} addTodos = {addTodos} handleKey = {handleKey} />
-      <TodoList todos = {todos} todoCompleted = {todoCompleted} deleteTheTodo = {deleteTheTodo} saveEdit = {saveEdit} startEditing = {startEditing} value = {editText} setEditText = {setEditText} editTodo = {editTodo} />
+   < div>
+   <TodoInput addInputOnSearch = {addInputOnSearch} value = {addInput} addTodo = {addTodo} handleKey = {handleKey}  />
+   <TodoList todos = {todos} todoCompleted = {todoCompleted} deleteTheTodo = {deleteTheTodo} />
     </div>
   )
 }
--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,13 +1,13 @@
- const TodoInput = ({addInputOnSearch, value, addTodos, handleKey}) => {
+ const TodoInput = ({addInputOnSearch ,value, addTodo, handleKey}) => {
   return (
     <div className="flex justify-center mt-6">
       <div className="w-full max-w-md flex gap-3">
         <input
-          type="text" onChange={addInputOnSearch} value={value} onKeyDown={handleKey}
+          type="text"  onChange={addInputOnSearch} value={value} onKeyDown={handleKey}
           placeholder="Add a todo..."
           className="flex-1 p-3 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
-        <button className="px-5 py-3 bg-blue-600 text-white rounded hover:bg-blue-700 cursor-pointer" onClick={addTodos}>
+        <button className="px-5 py-3 bg-blue-600 text-white rounded hover:bg-blue-700 cursor-pointer" onClick={addTodo} >
           Add
         </button>
       </div>
